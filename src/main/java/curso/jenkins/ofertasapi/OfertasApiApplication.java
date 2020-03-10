a21b9828a1bc33b72d3ca44b1675a584aa8c6bc8
AngelVil92 <angelvil92@gmail.com>
2020-03-10 17:01:42 +0100

    Initial commit

---
+++ src/main/java/curso/jenkins/ofertasapi/OfertasApiApplication.java
@@ -0,0 +1,13 @@
+package curso.jenkins.ofertasapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfertasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfertasApiApplication.class, args);
+	}
+
+}
